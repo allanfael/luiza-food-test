6fcd24a548463842c77d896dcbf0be1b1a413198
Allan Rafael <allan_fael@hotmail.com>
2025-04-29 14:33:04 -0300

    fix: floating button and footer

--- src/app/(project)/order/components/footer.tsx
+++ src/app/(project)/order/components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="flex flex-col gap-4 items-center mt-6 mb-20 px-6">
+    <div className="flex flex-col gap-4 items-center mt-6 pb-20 px-6">
       <p className="text-sm text-purple-800 font-semibold">feito com 💜 em maringá-PR</p>
     </div>
   )
